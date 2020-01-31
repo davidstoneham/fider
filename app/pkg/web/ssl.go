a1f6d54ad1a9fbf50aa294604f52223d07694c7f
davidstoneham <dstoneham.93@gmail.com>
2020-01-31 19:40:57 +1000

    Update ssl.go

--- app/pkg/web/ssl.go
+++ app/pkg/web/ssl.go
@@ -15,25 +15,7 @@ import (
 
 func getDefaultTLSConfig() *tls.Config {
 	return &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		insecureSkipVerify: true,
-		PreferServerCipherSuites: true,
-		CurvePreferences: []tls.CurveID{
-			tls.X25519,
-			tls.CurveP256,
-		},
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-		},
+		InsecureSkipVerify: true
 	}
 }
 
